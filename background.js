7ff5a68cee26d33b8c2c19408440ec1e1c663017
Jeremy Lee <jeremylee@Jeremys-MacBook-Pro.local>
2021-03-01 17:02:43 -0500

    Added working slideshow

--- background.js
+++ background.js
@@ -1,28 +1,49 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector(".prev").addEventListener("click", () => {
+    plusSlides(-1)
+  })
+  document.querySelector(".next").addEventListener("click", () => {
+    plusSlides(1)
+  })
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  document.querySelector("#dot1").addEventListener("click", () => {
+      currentSlide(1)
+  })
+  document.querySelector("#dot2").addEventListener("click", () => {
+    currentSlide(2)
+  })
+  document.querySelector("#dot3").addEventListener("click", () => {
+    currentSlide(3)
+  })
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+  var slideIndex = 1;
+    showSlides(slideIndex);
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides((slideIndex += n));
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides((slideIndex = n));
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {
+          slideIndex = 1;
+        }
+        if (n < 1) { 
+          slideIndex = slides.length;
+        }
+        Array.from(slides).forEach(item => item.style.display = "none");
+        Array.from(dots).forEach(
+          item => (item.className = item.className.replace(" active",''))
+        ); 
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    }
+})
